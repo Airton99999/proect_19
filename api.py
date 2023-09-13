60da82ec65384f40c596e7d61ed2801d0eba78ea
Airton99999 <118818956+Airton99999@users.noreply.github.com>
2023-09-13 04:07:53 +0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,72 @@
+import requests
+import json
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+
+
+class PetFriends:
+    """апи библиотека к веб приложению Pet Friends"""
+
+    def __init__(self):
+        self.base_url = "https://petfriends.skillfactory.ru"
+
+    def get_api_key(self, email: str, passwd: str) -> json:
+        """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате
+        JSON с уникальным ключем пользователя, найденного по указанным email и паролем"""
+
+        headers = {
+            'email': email,
+            'password': passwd,
+        }
+        res = requests.get(self.base_url+'/api/key', headers=headers)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+
+    def post_api_create_pet_simple(self, auth_key: json, name: str, animal_type: str, age: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
+        запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age,
+                  })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + '/api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
+
+    def post_add_photo(self, auth_key: json, pet_id: str, pet_photo: str) -> json:
+        """Этот метод позволяет добавить фотографию домашнего животного"""
+
+        data = MultipartEncoder(
+            fields={
+                'pet_id': pet_id,
+                'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
+            })
+
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + '/api/pets/set_photo/{pet_id}' + pet_id, headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
